4ddc5f1e541567bdde8dfd67dcdd3c7025278950
scottcagno <scottiecagno@gmail.com>
2014-10-29 10:33:05 -0400

    first commit

---
+++ src/main/groovy/com/cagnosolutions/data/Bootstrap.groovy
@@ -0,0 +1,23 @@
+package com.cagnosolutions.data
+
+import org.vertx.groovy.core.Vertx
+
+/**
+ * Created by Scott Cagno.
+ * Copyright Cagno Solutions. All rights reserved.
+ */
+
+class BootStrap {
+    def init = { servletContext ->
+        def vertx = Vertx.newVertx()
+        def httpServer = vertx.createHttpServer()
+        vertx.createSockJSServer(httpServer).installApp(prefix: '/events') { sock ->
+            sock.dataHandler { buff ->
+                sock << buff
+            }
+        }
+        httpServer.listen(8585)
+    }
+    def destroy = {
+    }
+}
